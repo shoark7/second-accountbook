3186d477dc84dc5e775296f79ba4f9690aab8928
Sunghwan Park <shoark7@gmail.com>
2016-12-26 13:41:33 +0900

    Implement add and save data method in DailyACcountBook
    1. add_data method creates new records.
    2. save_data method saves records into json file.

--- apis/daily_account_book.py
+++ apis/daily_account_book.py
@@ -21,7 +21,8 @@ from datetime import datetime
 import json
 import os
 from .base_account_book import BaseAccountBook
-from .help_method import get_weekday, read_day_file
+from .help_method import get_weekday, get_path_day, get_path_month, get_path_year
+from .record import Record
 
 TODAY = datetime.today()
 
@@ -36,25 +37,44 @@ class DailyAccountBook(BaseAccountBook):
         self.year = year
         self.month = month
         self.day = day
-        self.records = []
+        self.get_dict()
+        self.read_data()
 
     def read_data(self):
         """Read data from dataset json files"""
-        return read_day_file(year=self.year,
-                             month=self.month,
-                             day=self.day)
+        path = get_path_day(self.year, self.month, self.day)
+        if os.path.exists(path):
+            with open(path, 'r') as fp:
+                self.data = json.load(fp,)
+            return None
 
     def save_data(self):
         """Save data into dataset json files"""
-        raise NotImplementedError()
+        path = get_path_day(self.year, self.month, self.day)
+        path_month = get_path_month(self.year, self.month)
+        path_year = get_path_year(self.year)
+
+        if os.path.exists(path):
+            with open(path, 'w') as fp:
+                json.dump(self.data, fp,)
+        else:
+            if not os.path.exists(path_year):
+                os.mkdir(path_year)
+            if not os.path.exists(path_month):
+                os.mkdir(path_month)
+            with open(path, 'w') as fp:
+                json.dump(self.data, fp, )
 
     def average_data(self):
         """Average data depending on type of the book"""
         raise NotImplementedError()
 
-    def add_data(self):
+    def add_data(self, money, reason=None):
         """Add a new data entry into the book"""
-        raise NotImplementedError()
+        new_record = Record(money, reason)
+        self.data['count'] += 1
+        self.data['records'].append(new_record.get_dict())
+        return None
 
     def remove_data(self):
         """Remove data from the book"""
@@ -65,5 +85,13 @@ class DailyAccountBook(BaseAccountBook):
         raise NotImplementedError()
 
     def get_dict(self):
-        """Get dict version for serialization"""
-        raise NotImplementedError()
+        """Get dict version for first serialization"""
+        self.data = dict()
+        self.data['weekday'] = self.weekday
+        self.data['year'] = self.year
+        self.data['month'] = self.month
+        self.data['day'] = self.day
+        self.data['records'] = []
+        self.data['count'] = 0
+        return None
+
