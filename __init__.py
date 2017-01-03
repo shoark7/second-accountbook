ba1319d03b11987f6d456c4927384ae37b1c7421
Sunghwan Park <shoark7@gmail.com>
2017-01-03 21:11:23 +0900

    Set configuration for top-level __init__.py
    Now the trigger file(__main__.py) is being made.
    So I provide more convenient interface for it.
    I import apis and AccountBook.

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .account_book.account_book import AccountBook
+from .apis.daily_account_book import DailyAccountBook
+from .apis.monthly_account_book import MonthlyAccountBook
+from .apis.yearly_account_book import YearlyAccountBook
+from .apis import help_method
