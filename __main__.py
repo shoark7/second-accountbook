74e22e608ee255b75b4219d77169c8144da9c62d
Sunghwan Park <shoark7@gmail.com>
2017-01-05 15:00:28 +0900

    Add monthly and yearly statistic app in main
    App 3 has 2 monthly statistic functions.
    App 4 has 3 yearly statistic functions.
    
    App 4.2 is somewhat outstanding. Please check.

--- __main__.py
+++ __main__.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from Accountbook import AccountBook, help_method, DailyAccountBook,\
                         MonthlyAccountBook, YearlyAccountBook
 
-
 TODAY = datetime.today()
 
 
@@ -58,7 +57,6 @@ def app_start():
             print("감사합니다. 입력을 마칩니다.\n")
             book = AccountBook()
 
-        """ Function 2. """
         elif user_choice == "2":
             print("\n일일 통계를 지원합니다.\n원하는 날짜를 입력해주세요. 기본은 오늘입니다.")
             wanted_date = input("입력(ex. 2016/03/01) : ").strip()
@@ -74,12 +72,83 @@ def app_start():
                 except:
                     print("잘못 입력하셨습니다. 메인으로 돌아갑니다.\n\n")
 
-        """ Function 3."""
         elif user_choice == "3":
-            pass
+            print("\n월별 통계를 지원합니다.")
+            print("원하시는 연도, 월을 입력해주세요. 기본은 {}년 {}월입니다."\
+                  .format(TODAY.year, TODAY.month))
+            wanted_month = input("입력(ex. 2016/02) : \n")
+
+            if wanted_month == '':
+                year = TODAY.year
+                month = TODAY.month
+                wanted_book = MonthlyAccountBook(year, month)
+
+            else:
+                try:
+                    year, month = [int(e) for e in wanted_month.split('/')]
+                    wanted_book = MonthlyAccountBook(year, month)
+                except:
+                    print("잘못 입력하셨습니다. 메인으로 돌아갑니다.\n\n")
+                    continue
+
+            print("{}년 {}월의 통계를 출력합니다.".format(year, month))
+            print("원하시는 통계 유형을 선택해주세요.(기본: 1)\n")
+            print("1. 월 요약")
+            print("2. 월내 모든 데이터")
+            wanted_mode = input("\n입력 : ")
+
+            if wanted_mode == '2':
+                wanted_book.statistic_day_all()
+
+            else:
+                wanted_book.statistic_month()
 
         elif user_choice == "4":
-            pass
+            print("\n연도별 통계를 지원합니다.")
+            print("원하시는 연도를 입력해주세요. 기본은 {}년입니다.".format(TODAY.year))
+
+            wanted_year = input("입력(ex. 2016) : ")
+            print()
+
+            if wanted_year == '':
+                year = TODAY.year
+                wanted_book = YearlyAccountBook(year)
+
+            else:
+                try:
+                    year = int(wanted_year)
+                    wanted_book = YearlyAccountBook(year)
+                except:
+                    print("잘못 입력하셨습니다. 메인으로 돌아갑니다.\n\n")
+                    continue
+
+            print("{}년의 통계를 출력합니다.".format(year))
+            print("원하시는 통계 유형을 선택해주세요.(기본: 1)\n")
+
+            print("연도 요약 : 1")
+            print("모든 월 요약 : 2")
+            print("모든 월 출력 : 3")
+
+            wanted_mode = input("\n입력 : ").strip()
+
+            while True:
+                if wanted_mode not in ["1", "2", "3"]:
+                    wanted_mode = input("\n잘못된 입력입니다. 다시 입력하세요. : ").strip()
+                    continue
+
+                if wanted_mode == "1":
+                    wanted_book.statistic_year()
+                    break
+
+                elif wanted_mode == "2":
+                    wanted_book.statistic_month()
+                    break
+
+                else:
+                    wanted_book.statistic_month_all()
+                    break
+
+
 
         elif user_choice == "5":
             pass
